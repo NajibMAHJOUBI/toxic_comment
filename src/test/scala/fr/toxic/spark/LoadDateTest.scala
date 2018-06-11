fd49eaae6151df7bb481f198e53d392ff338bb19
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-11 17:22:26 +0200

    reformat

---
+++ src/test/scala/fr/toxic/spark/LoadDateTest.scala
@@ -0,0 +1,34 @@
+package fr.toxic.spark
+
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.{After, Before, Test}
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by mahjoubi on 11/06/18.
+  */
+
+class LoadDateTest extends AssertionsForJUnit {
+
+  private var spark: SparkSession = _
+
+  @Before def beforeAll() {
+    spark = SparkSession
+      .builder
+      .master("local")
+      .appName("test load dataset")
+      .getOrCreate()
+  }
+
+  @Test def testLoadDataSet(): Unit = {
+    print("test load dataset")
+    val ux = 1.0
+    assert(ux == 1.0)
+  }
+
+  @After def afterAll() {
+    spark.stop()
+  }
+
+
+}
