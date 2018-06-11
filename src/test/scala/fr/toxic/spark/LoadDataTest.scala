ee25e2f57067a14f5877d3dfc5a30bd9d6bb4eba
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-11 18:23:50 +0200

    loaddataset class first commit

--- src/test/scala/fr/toxic/spark/LoadDataTest.scala
+++ src/test/scala/fr/toxic/spark/LoadDataTest.scala
@@ -3,6 +3,9 @@ package fr.toxic.spark
 import org.scalatest.junit.AssertionsForJUnit
 import org.junit.{After, Before, Test}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StringType
+
+import fr.toxic.spark.LoadDataSet
 
 /**
   * Created by mahjoubi on 11/06/18.
@@ -20,10 +23,20 @@ class LoadDataTest extends AssertionsForJUnit {
       .getOrCreate()
   }
 
-  @Test def testLoadDataSet(): Unit = {
-    print("test load dataset")
-    val ux = 1.0
-    assert(ux == 1.0)
+  @Test def testLoadTrainSet(): Unit = {
+    val data = new LoadDataSet().run(spark, "train")
+    val columns = data.columns
+    assert(columns.contains("id"))
+    assert(columns.contains("comment_text"))
+    assert(columns.contains("toxic"))
+    assert(columns.contains("severe_toxic"))
+    assert(columns.contains("obscene"))
+    assert(columns.contains("threat"))
+    assert(columns.contains("insult"))
+    assert(columns.contains("identity_hate"))
+    val dataSchema = data.schema
+    assert(dataSchema.fields(dataSchema.fieldIndex("id")).dataType == StringType)
+    assert(dataSchema.fields(dataSchema.fieldIndex("comment_text")).dataType == StringType)
   }
 
   @After def afterAll() {
