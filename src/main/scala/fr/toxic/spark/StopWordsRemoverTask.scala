b7d796fa98a6b1a22fb36a3c2782506f5e0c11c8
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-12 13:42:31 +0200

    stop words remover first commit

---
+++ src/main/scala/fr/toxic/spark/StopWordsRemoverTask.scala
@@ -0,0 +1,21 @@
+package fr.toxic.spark
+
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.sql.DataFrame
+
+/**
+  * Created by mahjoubi on 12/06/18.
+  */
+class StopWordsRemoverTask(val inputColumn: String = "words", val outputColumn: String = "words",
+                           val stopWords: Array[String] = StopWordsRemover.loadDefaultStopWords("english")) {
+
+  def run(data: DataFrame): DataFrame = {
+    stopWordsRemover(data, inputColumn, outputColumn, stopWords)
+  }
+
+  def stopWordsRemover(data: DataFrame, inputColumn: String, outputColumn: String, stopWords: Array[String]): DataFrame = {
+    val removed = new StopWordsRemover().setInputCol(inputColumn).setOutputCol("stop_words_removed").transform(data)
+    removed.drop(inputColumn).withColumnRenamed("stop_words_removed", outputColumn)
+  }
+
+}
---
+++ src/test/scala/fr/toxic/spark/StopWordsRemoverTest.scala
@@ -0,0 +1,44 @@
+package fr.toxic.spark
+
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.sql.types.{ArrayType, StringType}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.junit.{After, Before, Test}
+import org.scalatest.junit.AssertionsForJUnit
+
+import scala.collection.mutable.WrappedArray
+
+/**
+  * Created by mahjoubi on 12/06/18.
+  */
+class StopWordsRemoverTest extends AssertionsForJUnit  {
+
+  private var spark: SparkSession = _
+
+  @Before def beforeAll() {
+    spark = SparkSession
+      .builder
+      .master("local")
+      .appName("test load dataset")
+      .getOrCreate()
+  }
+
+  @Test def testStopWordsRemover(): Unit = {
+    val data = new LoadDataSet("/home/mahjoubi/Documents/github/toxic_comment/src/test/ressources/data")
+      .run(spark, "train")
+    val tokens = new TokenizerTask().run(data)
+    val removed = new StopWordsRemoverTask().run(tokens)
+
+    assert(removed.isInstanceOf[DataFrame])
+    assert(removed.columns.contains("words"))
+    assert(removed.schema.fields(removed.schema.fieldIndex("words")).dataType ==  ArrayType(StringType))
+
+    val stopWordsRemover = StopWordsRemover.loadDefaultStopWords("english")
+    val words = removed.rdd.map(x => x.getAs[WrappedArray[String]](x.fieldIndex("words"))).collect()
+    assert(stopWordsRemover.intersect(words).length == 0)
+  }
+
+  @After def afterAll() {
+    spark.stop()
+  }
+}
