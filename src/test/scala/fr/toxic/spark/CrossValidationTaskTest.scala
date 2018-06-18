8b3e36643aff645f8ff015121ef000c700645413
ubuntu <najib.mahjoubi@gmail.com>
2018-06-19 00:56:12 +0200

    first commit

---
+++ src/test/scala/fr/toxic/spark/CrossValidationTaskTest.scala
@@ -0,0 +1,37 @@
+package fr.toxic.spark
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.junit.{After, Before, Test}
+import org.scalatest.junit.AssertionsForJUnit
+
+/**
+  * Created by mahjoubi on 12/06/18.
+  */
+class CrossValidationTaskTest extends AssertionsForJUnit {
+
+  private var spark: SparkSession = _
+
+  @Before def beforeAll() {
+    spark = SparkSession
+      .builder
+      .master("local")
+      .appName("test load dataset")
+      .getOrCreate()
+  }
+
+  @Test def crossValidationTest(): Unit = {
+    val data = new LoadDataSetTask("src/test/resources/data").run(spark, "binaryRelevance")
+    val columns = Array("toxic")
+    val savePath = "target/model/binaryRelevance/OneColumn"
+    new BinaryRelevanceTask(columns = columns, savePath = savePath).run(data)
+    val prediction = spark.read.option("header", "true").csv(s"${savePath}/prediction")
+    assert(prediction.isInstanceOf[DataFrame])
+    columns.map(column => assert(prediction.columns.contains(s"label_${column}")))
+    columns.map(column => assert(prediction.columns.contains(s"prediction_${column}")))
+    }
+
+  @After def afterAll() {
+    spark.stop()
+  }
+
+}
