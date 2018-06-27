0e37b9bdaa4db22fbf7b71ef502e67902b8ab0eb
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-27 17:57:39 +0200

    add logger

--- src/test/scala/fr/toxic/spark/BinaryRelevanceLogisticRegressionTaskTest.scala
+++ src/test/scala/fr/toxic/spark/BinaryRelevanceLogisticRegressionTaskTest.scala
@@ -1,5 +1,6 @@
 package fr.toxic.spark
 
+import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.junit.{After, Before, Test}
 import org.scalatest.junit.AssertionsForJUnit
@@ -17,12 +18,14 @@ class BinaryRelevanceLogisticRegressionTaskTest extends AssertionsForJUnit {
       .master("local")
       .appName("test load dataset")
       .getOrCreate()
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
   }
 
-  @Test def testBrOneColumnTest(): Unit = {
+  @Test def testBrOneColumnSimpleValidationTest(): Unit = {
     val data = new LoadDataSetTask("src/test/resources/data").run(spark, "tfIdf")
     val columns = Array("toxic")
-    val savePath = "target/model/binaryRelevance/OneColumn"
+    val savePath = "target/model/binaryRelevance/oneColumn/simpleValidation"
     new BinaryRelevanceLogisticRegressionTask(columns = columns, savePath = savePath,
                                               featureColumn = "tf_idf", methodValidation = "simple").run(data)
     val prediction = spark.read.option("header", "true").csv(s"${savePath}/prediction")
@@ -31,11 +34,24 @@ class BinaryRelevanceLogisticRegressionTaskTest extends AssertionsForJUnit {
     columns.map(column => assert(prediction.columns.contains(s"prediction_${column}")))
     }
 
-  @Test def testBrTwoColumnTest(): Unit = {
+  @Test def testBrOneColumnCrossValidationTest(): Unit = {
+    val data = new LoadDataSetTask("src/test/resources/data").run(spark, "tfIdf")
+    val columns = Array("toxic")
+    val savePath = "target/model/binaryRelevance/oneColumn/crossValidation"
+    new BinaryRelevanceLogisticRegressionTask(columns = columns, savePath = savePath,
+      featureColumn = "tf_idf", methodValidation = "cross_validation").run(data)
+    val prediction = spark.read.option("header", "true").csv(s"${savePath}/prediction")
+    assert(prediction.isInstanceOf[DataFrame])
+    columns.map(column => assert(prediction.columns.contains(s"label_${column}")))
+    columns.map(column => assert(prediction.columns.contains(s"prediction_${column}")))
+  }
+
+  @Test def testBrTwoColumnSimpleValidationTest(): Unit = {
     val data = new LoadDataSetTask("src/test/resources/data").run(spark, "tfIdf")
     val columns = Array("toxic", "severe_toxic")
-    val savePath = "target/model/binaryRelevance/twoColumn"
-    new BinaryRelevanceLogisticRegressionTask(columns = columns, savePath = savePath).run(data)
+    val savePath = "target/model/binaryRelevance/twoColumn/simpleValidation"
+    new BinaryRelevanceLogisticRegressionTask(columns = columns, savePath = savePath,
+                                              featureColumn = "tf_idf", methodValidation = "simple").run(data)
     val prediction = spark.read.option("header", "true").csv(s"${savePath}/prediction")
     assert(prediction.isInstanceOf[DataFrame])
     columns.map(column => assert(prediction.columns.contains(s"prediction_${column}")))
