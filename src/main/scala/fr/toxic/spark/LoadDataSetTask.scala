4f40f15d720d31b67f46859574a4915e4eb3c33e
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-28 14:50:11 +0200

    introduce an option to deal with different format of loading

--- src/main/scala/fr/toxic/spark/LoadDataSetTask.scala
+++ src/main/scala/fr/toxic/spark/LoadDataSetTask.scala
@@ -5,10 +5,17 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 /**
   * Created by mahjoubi on 11/06/18.
   */
-class LoadDataSetTask(val sourcePath: String) {
+class LoadDataSetTask(val sourcePath: String, val format: String = "parquet") {
+
+  private var data: DataFrame = _
 
   def run(spark: SparkSession, dataset: String): DataFrame = {
-    spark.read.parquet(s"${sourcePath}/${dataset}")
+    if (format =="csv") {
+      data = spark.read.option("header", "true").csv(s"${sourcePath}/${dataset}")
+    } else {
+      data = spark.read.parquet(s"${sourcePath}/${dataset}")
+    }
+    data
   }
 
 }
