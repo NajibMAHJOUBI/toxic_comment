224e8de005bf7e3fb0d51ef3e948aa6120f96ee5
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-06-29 17:44:23 +0200

    first commit

---
+++ src/main/scala/fr/toxic/spark/LinearSvcTask.scala
@@ -0,0 +1,69 @@
+package fr.toxic.spark
+
+import org.apache.spark.ml.classification.{LinearSVC, LinearSVCModel}
+import org.apache.spark.sql.DataFrame
+
+/**
+  * Created by mahjoubi on 12/06/18.
+  *
+  * LinearSVC classifier
+  *
+  */
+class LinearSvcTask(val labelColumn: String = "label",
+                    val featureColumn: String = "features",
+                    val predictionColumn: String = "prediction") {
+
+
+  var model: LinearSVC = _
+  var modelFit: LinearSVCModel = _
+  var transform: DataFrame = _
+
+  def getModelFit(): LinearSVCModel = {
+    modelFit
+  }
+
+  def defineModel(): LinearSvcTask= {
+    model = new LinearSVC()
+      .setFeaturesCol(featureColumn)
+      .setLabelCol(labelColumn)
+      .setPredictionCol(predictionColumn)
+    this
+  }
+
+  def fit(data: DataFrame): LinearSvcTask = {
+    modelFit = getModel().fit(data)
+    this
+  }
+
+  def getModel(): LinearSVC = {
+    model
+  }
+
+  def transform(data: DataFrame): LinearSvcTask = {
+    transform = modelFit.transform(data)
+    this
+  }
+
+  def saveModel(path: String): LinearSvcTask = {
+    model.save(path)
+    this
+  }
+
+  def setRegParam(value: Double): LinearSvcTask = {
+    model.setRegParam(value)
+    this
+  }
+
+  def getRegParam(): Double = {
+    model.getRegParam
+  }
+
+  def loadModel(path: String): LinearSvcTask = {
+    modelFit = LinearSVCModel.load(path)
+    this
+  }
+
+  def getTransform(): DataFrame = {
+    transform
+  }
+}
