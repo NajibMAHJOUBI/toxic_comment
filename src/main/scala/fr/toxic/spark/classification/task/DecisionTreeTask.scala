4f2b6d94541ad428fd37002c77575f3095f9e6cc
ubuntu <najib.mahjoubi@gmail.com>
2018-07-01 14:40:43 +0200

    first commit

---
+++ src/main/scala/fr/toxic/spark/classification/task/DecisionTreeTask.scala
@@ -0,0 +1,55 @@
+package fr.toxic.spark.classification.task
+
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.sql.DataFrame
+
+class DecisionTreeTask (val labelColumn: String = "label",
+                        val featureColumn: String = "features",
+                        val predictionColumn: String = "prediction") extends ClassificationModelFactory {
+
+  var model: DecisionTreeClassifier = _
+  var modelFit: DecisionTreeClassificationModel = _
+  var transform: DataFrame = _
+
+  override def defineModel(): DecisionTreeTask= {
+    model = new DecisionTreeClassifier()
+      .setFeaturesCol(featureColumn)
+      .setLabelCol(labelColumn)
+      .setPredictionCol(predictionColumn)
+    this
+  }
+
+  override def fit(data: DataFrame): LinearSvcTask = {
+    modelFit = getModel.fit(data)
+    this
+  }
+
+  def getModel: DecisionTreeClassifier = {
+    model
+  }
+
+  override def transform(data: DataFrame): DecisionTreeTask = {
+    transform = modelFit.transform(data)
+    this
+  }
+
+  override def loadModel(path: String): DecisionTreeTask = {
+    modelFit = DecisionTreeClassificationModel.load(path)
+    this
+  }
+
+  override def saveModel(path: String): DecisionTreeTask = {
+    model.save(path)
+    this
+  }
+
+  def getModelFit: DecisionTreeClassificationModel = {
+    modelFit
+  }
+
+  def getTransform: DataFrame = {
+    transform
+  }
+
+}
