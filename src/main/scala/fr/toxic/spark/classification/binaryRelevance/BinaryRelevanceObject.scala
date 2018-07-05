093c1f818de20d7bdbad99d66d8f38a5ccc1c2d3
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-05 16:52:44 +0200

    private data for all test functions

--- src/main/scala/fr/toxic/spark/classification/binaryRelevance/BinaryRelevanceObject.scala
+++ src/main/scala/fr/toxic/spark/classification/binaryRelevance/BinaryRelevanceObject.scala
@@ -1,11 +1,16 @@
 package fr.toxic.spark.classification.binaryRelevance
 
 import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.DataFrame
 
 object BinaryRelevanceObject {
 
   def getPredictionProbability(probability: Vector, prediction: Double): Double = {
     probability(prediction.toInt)
+  }
+
+  def createLabel(data: DataFrame, column: String): DataFrame = {
+    data.withColumnRenamed(column, s"label_$column")
   }
 
 }
