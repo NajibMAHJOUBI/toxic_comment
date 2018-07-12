f2c002ad524c5668f4b88348a43fd81786d0b331
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-12 10:58:54 +0200

    refactoring to use trait and extends class

---
+++ src/main/scala/fr/toxic/spark/text/featurization/TextFeaturizationTask.scala
@@ -0,0 +1,13 @@
+package fr.toxic.spark.text.featurization
+
+import org.apache.spark.sql.DataFrame
+
+class TextFeaturizationTask {
+
+   var prediction: DataFrame = _
+
+   def getPrediction: DataFrame = {
+    prediction
+  }
+
+}
