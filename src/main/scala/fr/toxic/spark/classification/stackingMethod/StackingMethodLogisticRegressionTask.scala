700c6cbc582387d4349d56b6f1c111453420a258
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-12 10:57:03 +0200

    move methods loadDataPredictionByLabel, loadDataLabel, mergeData, createLabelFeatures to classStackingMethod

--- src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
+++ src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
@@ -1,11 +1,12 @@
 package fr.toxic.spark.classification.stackingMethod
 
-import fr.toxic.spark.utils.LoadDataSetTask
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.col
-import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.sql.SparkSession
 
-class StackingMethodLogisticRegressionTask(labels: Array[String], classificationMethods: Array[String], pathLabel: String, pathPrediction: String) {
+class StackingMethodLogisticRegressionTask(override val labels: Array[String],
+                                           override val classificationMethods: Array[String],
+                                           override val pathLabel: String,
+                                           override val pathPrediction: String)
+  extends StackingMethodTask(labels, classificationMethods, pathLabel, pathPrediction) {
 
   def run(spark: SparkSession): StackingMethodLogisticRegressionTask = {
     labels.foreach(label => {
@@ -19,28 +20,7 @@ class StackingMethodLogisticRegressionTask(labels: Array[String], classification
     this
   }
 
-  def loadDataPredictionByLabel(spark: SparkSession, method: String, label: String): DataFrame = {
-    new LoadDataSetTask(s"$pathPrediction/$method", format= "csv")
-      .run(spark, "prediction")
-      .select(col("id"), col(s"prediction_$label"))
-  }
 
-  def loadDataLabel(spark: SparkSession, label: String): DataFrame = {
-    new LoadDataSetTask(sourcePath = pathLabel).run(spark, "train")
-      .select(col("id"), col(label).alias("label"))
-  }
-
-  def mergeData(spark: SparkSession, label: String): DataFrame = {
-    var data: DataFrame = loadDataLabel(spark, label)
-    classificationMethods.foreach(method => data = data.join(loadDataPredictionByLabel(spark, method, label), Seq("id")))
-    data.drop("id")
-  }
-
-  def createLabelFeatures(dataFrame: DataFrame): Vector = {
-    val u = Vectors.dense(Array(1.0, 2.0, 3.3))
-    print(u)
-    u
-  }
 
 
 
