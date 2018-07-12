c8836154530c436ea5b5e12f9dc71bb137ef7642
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-12 16:44:10 +0200

    add option to use lucene stop words

--- src/main/scala/fr/toxic/spark/text/featurization/StopWordsRemoverTask.scala
+++ src/main/scala/fr/toxic/spark/text/featurization/StopWordsRemoverTask.scala
@@ -2,20 +2,46 @@ package fr.toxic.spark.text.featurization
 
 import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.sql.DataFrame
+import org.apache.lucene.analysis.en.EnglishAnalyzer
 
 /**
   * Created by mahjoubi on 12/06/18.
+  *
+  * Stop words remover task
+  *
   */
+
+
 class StopWordsRemoverTask(val inputColumn: String = "words", val outputColumn: String = "words",
-                           val stopWords: Array[String] = StopWordsRemover.loadDefaultStopWords("english")) {
+                           val stopWordsOption: String) {
+
+  var stopWords: Array[String] = _
 
   def run(data: DataFrame): DataFrame = {
-    stopWordsRemover(data, inputColumn, outputColumn, stopWords)
+    defineStopWordsList()
+    stopWordsRemover(data)
   }
 
-  def stopWordsRemover(data: DataFrame, inputColumn: String, outputColumn: String, stopWords: Array[String]): DataFrame = {
-    val removed = new StopWordsRemover().setInputCol(inputColumn).setOutputCol("stop_words_removed").transform(data)
-    removed.drop(inputColumn).withColumnRenamed("stop_words_removed", outputColumn)
+  def stopWordsRemover(data: DataFrame): DataFrame = {
+    val removed = new StopWordsRemover()
+      .setInputCol(inputColumn)
+      .setOutputCol("stop_words_removed")
+      .setStopWords(stopWords)
+
+     removed.transform(data).drop(inputColumn).withColumnRenamed("stop_words_removed", outputColumn)
+  }
+
+  def defineStopWordsList(): StopWordsRemoverTask = {
+    stopWords = if (stopWordsOption == "lucene") {
+        new EnglishAnalyzer().getStopwordSet().toString.split(",")
+    } else {
+      StopWordsRemover.loadDefaultStopWords("english")
+    }
+    this
+  }
+
+  def getStopWords: Array[String] = {
+    stopWords
   }
 
 }
