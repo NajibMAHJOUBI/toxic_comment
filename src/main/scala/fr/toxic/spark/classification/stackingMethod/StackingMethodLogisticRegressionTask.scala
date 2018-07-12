e8765b053ec3d54d0d1ffae681eaeebede10a528
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-12 16:49:18 +0200

    add test

--- src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
+++ src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
@@ -6,22 +6,13 @@ class StackingMethodLogisticRegressionTask(override val labels: Array[String],
                                            override val classificationMethods: Array[String],
                                            override val pathLabel: String,
                                            override val pathPrediction: String)
-  extends StackingMethodTask(labels, classificationMethods, pathLabel, pathPrediction) {
+  extends StackingMethodTask(labels, classificationMethods, pathLabel, pathPrediction) with StackingMethodFactory {
 
-  def run(spark: SparkSession): StackingMethodLogisticRegressionTask = {
+  override def run(spark: SparkSession): StackingMethodLogisticRegressionTask = {
     labels.foreach(label => {
-      val data = mergeData(spark, label)
-
-
-
-
-
-    })
+      val data = mergeData(spark, label)})
+      data.show()
     this
   }
-
-
-
-
 
 }
