621f35e45ccf00d1e68fd37d8ac211d317c238a1
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-13 12:32:34 +0200

    add method getMlVector

--- src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodObject.scala
+++ src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodObject.scala
@@ -1,6 +1,9 @@
 package fr.toxic.spark.classification.stackingMethod
 
+import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.sql.Row
+
+import scala.collection.mutable.WrappedArray
 
 object StackingMethodObject {
 
@@ -8,5 +11,9 @@ object StackingMethodObject {
     var value: Array[Double] = Array()
     classificationMethods.foreach(method => value = value :+ p.getString(p.fieldIndex(s"prediction_$method")).toDouble)
     value
+  }
+
+  def getMlVector(values: WrappedArray[Double]): Vector = {
+    Vectors.dense(values.toArray[Double])
   }
 }
