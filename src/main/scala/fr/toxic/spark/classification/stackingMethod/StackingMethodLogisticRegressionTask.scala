7e49ac16bf6ac3469b08972707548418da58d6a6
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-13 12:33:19 +0200

    add methods saveModel, loadModel, computePrediction

--- src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
+++ src/main/scala/fr/toxic/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
@@ -1,14 +1,14 @@
 package fr.toxic.spark.classification.stackingMethod
 
 import fr.toxic.spark.classification.crossValidation.CrossValidationLogisticRegressionTask
+import fr.toxic.spark.classification.task.LogisticRegressionTask
 import org.apache.spark.ml.classification.LogisticRegressionModel
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+
 class StackingMethodLogisticRegressionTask(override val labels: Array[String],
                                            override val classificationMethods: Array[String],
-                                           override val pathLabel: String,
-                                           override val pathPrediction: String)
-  extends StackingMethodTask(labels, classificationMethods, pathLabel, pathPrediction) with StackingMethodFactory {
+                                           override val pathLabel: String, override val pathPrediction: String, override val pathSave: String) extends StackingMethodTask(labels, classificationMethods, pathLabel, pathPrediction, pathSave) with StackingMethodFactory {
 
   val labelColumn: String = "label"
   val featureColumn: String = "features"
@@ -17,8 +17,9 @@ class StackingMethodLogisticRegressionTask(override val labels: Array[String],
   override def run(spark: SparkSession): StackingMethodLogisticRegressionTask = {
     labels.foreach(label => {
       mergeData(spark, label)
-      val data = createDfLabelFeatures(spark, label)
+      val data = createLabelFeatures(spark, label)
       computeModel(data, label)
+      saveModel(label)
     })
 
     this
@@ -31,6 +32,21 @@ class StackingMethodLogisticRegressionTask(override val labels: Array[String],
       pathPrediction = "")
     cv.run()
     model = cv.getBestModel
+    this
+  }
+
+  override def saveModel(label: String): StackingMethodLogisticRegressionTask = {
+    model.write.overwrite().save(s"$pathSave/model/$label")
+    this
+  }
+
+  override def loadModel(path: String): StackingMethodLogisticRegressionTask = {
+    model = new LogisticRegressionTask(featureColumn = featureColumn).loadModel(path).getModelFit
+    this
+  }
+
+  override def computePrediction(data: DataFrame): StackingMethodLogisticRegressionTask = {
+    prediction = model.transform(data).drop(Seq("rawPrediction", "probability"): _*)
     this
   }
 
