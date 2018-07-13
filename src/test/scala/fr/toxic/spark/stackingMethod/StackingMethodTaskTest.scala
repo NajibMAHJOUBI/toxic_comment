6309110d862220e4b34f22d71a73428b7ada875d
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-13 12:31:33 +0200

    add assert test to chack the type of the createDfLabelFeatures method

--- src/test/scala/fr/toxic/spark/stackingMethod/StackingMethodTaskTest.scala
+++ src/test/scala/fr/toxic/spark/stackingMethod/StackingMethodTaskTest.scala
@@ -1,12 +1,10 @@
 package fr.toxic.spark.stackingMethod
 
-import fr.toxic.spark.classification.stackingMethod.{StackingMethodObject, StackingMethodTask}
-import fr.toxic.spark.utils.LoadDataSetTask
+import fr.toxic.spark.classification.stackingMethod.StackingMethodTask
 import org.apache.log4j.{Level, LogManager}
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.junit.{After, Before, Test}
-
 
 class StackingMethodTaskTest {
 
@@ -26,7 +24,7 @@ class StackingMethodTaskTest {
   }
 
   @Test def testLoadDataPredictionByLabel(): Unit = {
-    val stackingMethod = new StackingMethodTask(Array(""), Array(""), "", pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), Array(""), "", pathPrediction, "")
     val label = "toxic"
     val method = "logisticRegression"
     val data = stackingMethod.loadDataPredictionByLabel(spark, "logisticRegression", label)
@@ -37,7 +35,7 @@ class StackingMethodTaskTest {
   }
 
   @Test def testLoadDataLabel(): Unit = {
-    val stackingMethod = new StackingMethodTask(Array(""), Array(""), pathLabel, "")
+    val stackingMethod = new StackingMethodTask(Array(""), Array(""), pathLabel, "", "")
     val label = "toxic"
     val data  = stackingMethod.loadDataLabel(spark, label)
 
@@ -50,7 +48,7 @@ class StackingMethodTaskTest {
 
   @Test def testMergeData(): Unit = {
     val methodClassification = Array("logisticRegression", "randomForest")
-    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction, "")
     stackingMethod.mergeData(spark, "toxic")
     val data = stackingMethod.getData
 
@@ -63,14 +61,18 @@ class StackingMethodTaskTest {
   @Test def testCreateDfLabelFeatures(): Unit = {
     val methodClassification = Array("logisticRegression", "randomForest")
     val label = "toxic"
-    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction, "")
     stackingMethod.mergeData(spark, label)
-    val labelFeatures = stackingMethod.createDfLabelFeatures(spark, label)
+    val labelFeatures = stackingMethod.createLabelFeatures(spark, label)
 
     assert(labelFeatures.isInstanceOf[DataFrame])
     assert(labelFeatures.columns.length == 2)
     assert(labelFeatures.columns.contains(label))
     assert(labelFeatures.columns.contains("features"))
+
+    val dataSchema = labelFeatures.schema
+    assert(dataSchema.fields(dataSchema.fieldIndex(label)).dataType == LongType)
+    assert(dataSchema.fields(dataSchema.fieldIndex("features")).dataType.typeName == "vector")
   }
 
   @After def afterAll() {
