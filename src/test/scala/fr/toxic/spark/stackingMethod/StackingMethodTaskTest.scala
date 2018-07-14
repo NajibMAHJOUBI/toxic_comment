925bfacada3abaecf7769e02abf1fd0098a77532
ubuntu <najib.mahjoubi@gmail.com>
2018-07-14 12:48:05 +0200

    add assert test for creatreLabelMeatures method

--- src/test/scala/fr/toxic/spark/stackingMethod/StackingMethodTaskTest.scala
+++ src/test/scala/fr/toxic/spark/stackingMethod/StackingMethodTaskTest.scala
@@ -2,6 +2,7 @@ package fr.toxic.spark.stackingMethod
 
 import fr.toxic.spark.classification.stackingMethod.StackingMethodTask
 import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.sql.types.LongType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.junit.{After, Before, Test}
 
@@ -24,7 +25,7 @@ class StackingMethodTaskTest {
   }
 
   @Test def testLoadDataPredictionByLabel(): Unit = {
-    val stackingMethod = new StackingMethodTask(Array(""), Array(""), "", pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), Array(""), "", pathPrediction, "")
     val label = "toxic"
     val method = "logisticRegression"
     val data = stackingMethod.loadDataPredictionByLabel(spark, "logisticRegression", label)
@@ -35,7 +36,7 @@ class StackingMethodTaskTest {
   }
 
   @Test def testLoadDataLabel(): Unit = {
-    val stackingMethod = new StackingMethodTask(Array(""), Array(""), pathLabel, "")
+    val stackingMethod = new StackingMethodTask(Array(""), Array(""), pathLabel, "", "")
     val label = "toxic"
     val data = stackingMethod.loadDataLabel(spark, label)
 
@@ -48,7 +49,7 @@ class StackingMethodTaskTest {
 
   @Test def testMergeData(): Unit = {
     val methodClassification = Array("logisticRegression", "randomForest")
-    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction, "")
     stackingMethod.mergeData(spark, "toxic")
     val data = stackingMethod.getData
 
@@ -58,18 +59,21 @@ class StackingMethodTaskTest {
     methodClassification.foreach(method => assert(data.columns.contains(s"prediction_$method")))
   }
 
-  @Test def testCreateDfLabelFeatures(): Unit = {
+  @Test def testCreateLabelFeatures(): Unit = {
     val methodClassification = Array("logisticRegression", "randomForest")
     val label = "toxic"
-    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction)
+    val stackingMethod = new StackingMethodTask(Array(""), methodClassification, pathLabel, pathPrediction, "")
     stackingMethod.mergeData(spark, label)
-    val labelFeatures = stackingMethod.createDfLabelFeatures(spark, label)
+    val labelFeatures = stackingMethod.createLabelFeatures(spark, label)
     labelFeatures.show()
 
     assert(labelFeatures.isInstanceOf[DataFrame])
     assert(labelFeatures.columns.length == 2)
     assert(labelFeatures.columns.contains(label))
     assert(labelFeatures.columns.contains("features"))
+    val dataSchema = labelFeatures.schema
+    assert(dataSchema.fields(dataSchema.fieldIndex(label)).dataType == LongType)
+    assert(dataSchema.fields(dataSchema.fieldIndex("features")).dataType.typeName == "vector")
   }
 
   @After def afterAll() {
