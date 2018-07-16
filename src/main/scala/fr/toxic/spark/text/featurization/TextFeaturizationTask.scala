102cf357bf605108a80c7f4bc6996dfd96452f10
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-16 16:57:56 +0200

    add method logVector

--- src/main/scala/fr/toxic/spark/text/featurization/TextFeaturizationTask.scala
+++ src/main/scala/fr/toxic/spark/text/featurization/TextFeaturizationTask.scala
@@ -1,6 +1,9 @@
 package fr.toxic.spark.text.featurization
 
+import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.sql.DataFrame
+
+import scala.math.log
 
 class TextFeaturizationTask {
 
@@ -8,6 +11,13 @@ class TextFeaturizationTask {
 
   def getPrediction: DataFrame = {
     prediction
+  }
+
+  def logVector(u: Vector): Vector = {
+    val size = u.size
+    val indices = u.toSparse.indices
+    val values = u.toSparse.values.map((x: Double) => log(x))
+    Vectors.sparse(size, indices, values)
   }
 
 }
